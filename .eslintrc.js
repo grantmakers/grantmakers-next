fe0088a3ffcfabf829acb9322fa0b0a941802a3c
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-02-04 17:31:26 -0600

    Let eslint handle no-unused-vars [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['apps/**', 'packages/**'],
+  extends: ['@repo/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    // ... rest
   },
 };
