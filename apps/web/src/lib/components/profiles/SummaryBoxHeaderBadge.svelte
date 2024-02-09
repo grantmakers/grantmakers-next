e6d687bf965b179afee5f8b7d83419bd24eeabf6
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-02-09 10:46:55 -0600

    Let Turborepo handle formatting

--- apps/web/src/lib/components/profiles/SummaryBoxHeaderBadge.svelte
+++ apps/web/src/lib/components/profiles/SummaryBoxHeaderBadge.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <span
-  class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset {variant === 'positive'
-    ? positiveClasses
-    : negativeClasses}"
+  class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset {variant === 'positive' ? positiveClasses : (
+    negativeClasses
+  )}"
 >
   <slot />
 </span>
