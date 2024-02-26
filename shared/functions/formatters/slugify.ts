7acf1bc94cf4bed8e57614340200e908237c7c55
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-02-26 13:15:28 -0600

    Add slugify helper function

---
+++ shared/functions/formatters/slugify.ts
@@ -0,0 +1,20 @@
+import { default as npmSlugify } from 'slugify';
+
+export function slugify(string: string): string {
+  const options = {
+    replacement: '-',
+    remove: undefined,
+    lower: true,
+    strict: true, // slug.replace(/[^A-Za-z0-9\s]/g, '')
+    trim: true,
+  };
+
+  // Slugify only handles characters, not substrings
+  // Thus, need to define a preprocessor to process HTML entities
+  // Note: This is now captured upstream in the ETL. Including here to handle legacy edge cases.
+  const cleaned = string.replace('&amp;', '&');
+
+  const slug = npmSlugify(cleaned, options);
+  console.log(slug);
+  return slug;
+}
