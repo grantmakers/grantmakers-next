801f2d48521dcaf7ed6336839153f025b781fa22
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-03-03 18:43:25 -0600

    Enabled prettier on /api

--- apps/api/src/r2/index.ts
+++ apps/api/src/r2/index.ts
@@ -3,10 +3,10 @@ export async function fetchR2Object(env: Env, key: string): Promise<Response> {
   const profile = await env.STATIC_API_V1.get(key);
 
   if (!profile) {
-    return new Response("Profile Not Found", { status: 404 });
+    return new Response('Profile Not Found', { status: 404 });
   }
 
   return new Response(profile.body, {
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 }
--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -6,9 +6,9 @@
  * 1) Route patterns
  * 2) Chaining the route patterns
  */
-import { Router } from "itty-router";
-import { fetchR2Object } from "./r2";
-import { isValidEin } from "./utils/validators";
+import { Router } from 'itty-router';
+import { fetchR2Object } from './r2';
+import { isValidEin } from './utils/validators';
 
 // eslint-disable-next-line new-cap
 const router = Router();
@@ -18,22 +18,22 @@ const endpointGetProfileObject = "/api/profiles/v1/:ein";
 
 // Use itty-router's chain feature to define all handlers
 router
-  .get(endpointProfilesIndex, () => new Response("Profiles Index coming soon"))
+  .get(endpointProfilesIndex, () => new Response('Profiles Index coming soon'))
 
   .get(endpointGetProfileObject, async (request, env) => {
     const { params } = request;
     const ein = params.ein;
-    const key = ein + ".json";
+    const key = ein + '.json';
 
     // Validate EIN
     if (isValidEin(ein)) {
       return fetchR2Object(env, key);
     }
 
-    return new Response("Invalid EIN format", { status: 400 });
+    return new Response('Invalid EIN format', { status: 400 });
   })
 
   // 404 for everything else
-  .all("*", () => new Response("Not Found.", { status: 404 }));
+  .all('*', () => new Response('Not Found.', { status: 404 }));
 
 export default router;
