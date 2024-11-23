95222d2d0420138283bc57fa9748ff2dc0e2bc00
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-11-22 20:27:17 -0600

    Finally found the linter fix 🎉

--- apps/web/.prettierrc.cjs
+++ apps/web/.prettierrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
   singleQuote: true,
   svelteIndentScriptAndStyle: true,
+  tailwindConfig: './tailwind.config.js',
   overrides: [
     {
       files: '*.svelte',
