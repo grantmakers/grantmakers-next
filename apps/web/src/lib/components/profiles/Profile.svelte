3ce7691f5a5662bbdfebf07a19fe0e486cfe750b
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-11-25 09:42:09 -0600

    Section styling consitency

--- apps/web/src/lib/components/profiles/Profile.svelte
+++ apps/web/src/lib/components/profiles/Profile.svelte
@@ -203,8 +203,8 @@
         <!-- Grants -->
         <div class="-mx-3 grid grid-cols-1">
           <div class="mb-4 w-full max-w-full px-3">
-            <div class="shadow-soft-xl relative flex h-full min-w-0 flex-col break-words rounded-2xl border-0 bg-white bg-clip-border">
-              <div class="mb-0 rounded-t-2xl border-b-0 bg-slate-200 p-4">
+            <div class="shadow-soft-xl relative flex h-full min-w-0 flex-col overflow-hidden break-words rounded-2xl bg-white">
+              <div class="mb-0 bg-slate-200 p-4">
                 <SummaryBoxHeader headerText={'Grants'}
                   ><img src={irsLogo} alt="IRS logo" class="max-h-6 w-full" height={24} width={48} /></SummaryBoxHeader
                 >
@@ -230,8 +230,8 @@
         <!-- Charitable Activities -->
         <div class="-mx-3 grid grid-cols-1">
           <div class="mb-4 w-full max-w-full px-3">
-            <div class="shadow-soft-xl relative flex h-full min-w-0 flex-col break-words rounded-2xl border-0 bg-white bg-clip-border">
-              <div class="mb-0 rounded-t-2xl border-b-0 bg-slate-200 p-4">
+            <div class="shadow-soft-xl relative flex h-full min-w-0 flex-col overflow-hidden break-words rounded-2xl bg-white">
+              <div class="mb-0 border-b-0 bg-slate-200 p-4">
                 <SummaryBoxHeader headerText={'Charitable Activities'}
                   ><img src={irsLogo} alt="IRS logo" class="max-h-6 w-full" height={24} width={48} /></SummaryBoxHeader
                 >
