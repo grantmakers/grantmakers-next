812c7b839af33b70f6dc93238234a65e93b15e0f
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-11-25 09:24:53 -0600

    Bottom CTA mobile fix

--- apps/web/src/lib/components/profiles/Profile.svelte
+++ apps/web/src/lib/components/profiles/Profile.svelte
@@ -339,7 +339,7 @@
             <div class="flex flex-col items-center justify-center gap-2">
               <div class="text-lg font-semibold">Looking for another grantmaker?</div>
               <div class="text-sm">Free profiles for over 150,000 US private foundations</div>
-              <div class="mt-4 flex flex-wrap items-center gap-4">
+              <div class="mt-4 flex flex-col flex-wrap items-center gap-4 md:flex-row">
                 <button
                   type="button"
                   class="inline-flex items-center gap-x-1.5 rounded-md bg-grantmakers-orange px-3 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- apps/web/src/lib/components/profiles/topnav/SurpriseMe.svelte
+++ apps/web/src/lib/components/profiles/topnav/SurpriseMe.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a class="pr-3 text-xs font-semibold uppercase text-slate-500" href="/profiles/random">
-  <div class="hidden flex-row items-center gap-2 lg:flex">
+  <div class="flex flex-row items-center gap-2">
     <ArrowPath class="size-4 text-gray-500" />
     Surprise me
   </div>
