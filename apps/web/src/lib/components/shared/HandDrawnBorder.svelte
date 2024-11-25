c4712daf9876698508f26c942d2d0ae88d02a683
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-11-24 21:03:16 -0600

    Grants median TS nit

--- apps/web/src/lib/components/shared/HandDrawnBorder.svelte
+++ apps/web/src/lib/components/shared/HandDrawnBorder.svelte
@@ -7,7 +7,7 @@
 
   let { fill = 'fill-grantmakers-blue', children }: Props = $props();
 
-  type Fill = 'fill-grantmakers-orange' | 'fill-grantmakers-blue' | 'fill-grantmakers-green' | 'fill-transparent';
+  type Fill = 'fill-grantmakers-orange' | 'fill-yellow-500' | 'fill-grantmakers-blue' | 'fill-grantmakers-green' | 'fill-transparent';
 </script>
 
 <div class="relative">
