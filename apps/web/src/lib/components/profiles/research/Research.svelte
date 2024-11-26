9e95483d1c149b554f75a5d8578b47edbbedad70
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-11-26 14:18:06 -0600

    Improve research layout

--- apps/web/src/lib/components/profiles/research/Research.svelte
+++ apps/web/src/lib/components/profiles/research/Research.svelte
@@ -20,14 +20,23 @@
 </div>
 <div class="grid grid-cols-1 md:grid-cols-2 md:grid-rows-1">
   <div class="flex-auto items-start justify-center gap-2 p-8">
-    <div class="flex items-center rounded bg-slate-100 p-2">
-      <div class="flex grow flex-col gap-1">
-        <div class="text-xs text-green-500">Non-Commercial</div>
-        <div class="text-sm font-bold uppercase text-slate-700">Review the 990</div>
+    <div class="flex items-end justify-between rounded">
+      <div class="flex flex-col gap-3">
+        <div
+          class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+        >
+          Non-Commercial
+        </div>
+        <!-- <div class="text-xs uppercase text-green-500">Non-Commercial</div> -->
+        <div class="flex flex-row justify-start">
+          <img src={propublicaLogo} alt="ProPublica logo" class="h-4 w-auto object-contain" height={16} width={184} />
+        </div>
       </div>
-      <div class="shrink"><img src={propublicaLogo} alt="ProPublica logo" class="max-h-4 shrink" height={16} width={184} /></div>
+      <div class="flex flex-col gap-3">
+        <div class="text-sm font-semibold text-slate-700">Review the 990</div>
+      </div>
     </div>
-    <div class="mt-8 flex flex-wrap items-center gap-4">
+    <div class="mt-8 flex flex-wrap items-center justify-center gap-4 rounded-md bg-slate-100 p-4 md:justify-start">
       <a
         href="https://projects.propublica.org/nonprofits/organizations/{ein}/{irsObjectId}/full"
         target="_blank"
@@ -48,14 +57,21 @@
   </div>
 
   <div class="flex-auto items-start justify-center gap-4 p-8">
-    <div class="flex items-center rounded bg-slate-100 p-2">
-      <div class="flex grow flex-col gap-1">
-        <div class="text-xs text-blue-500">Government Source</div>
-        <div class="text-sm font-bold uppercase text-slate-700">Review IRS docs</div>
+    <div class="flex w-full items-end justify-between rounded">
+      <div class="flex flex-col gap-2">
+        <span
+          class="inline-flex items-center whitespace-nowrap rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10"
+        >
+          Government Source
+        </span>
+        <!-- <div class="whitespace-nowrap text-xs text-blue-500">Government Source</div> -->
+        <img src={irsLogo} alt="IRS logo" class="max-h-6" height={24} width={48} />
       </div>
-      <img src={irsLogo} alt="IRS logo" class="max-h-6" height={24} width={48} />
+      <div class="flex flex-col gap-4">
+        <div class="ml-8 text-sm font-semibold text-slate-700">Review Other IRS docs</div>
+      </div>
     </div>
-    <div class="mt-8 flex flex-wrap items-center gap-4">
+    <div class="mt-8 flex flex-wrap items-center justify-center gap-4 rounded-md bg-slate-100 p-4 md:justify-start">
       <a
         href="https://apps.irs.gov/app/eos/"
         target="_blank"
@@ -76,9 +92,9 @@
           }}
           type="button"
           class="flex cursor-copy flex-row items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-slate-700 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50"
-          ><ClipboardDocument variation={'solid'} class={'mr-2 h-4 w-4 text-slate-500'} /> EIN {ein}</button
+          ><ClipboardDocument variation={'solid'} class={'mr-2 h-4 w-4 text-slate-500'} /> Copy EIN</button
         >
-        <div class="text-xs">← Psst, copy this first</div>
+        <div class="text-xs">← Psst, click this first</div>
       </div>
     </div>
   </div>
