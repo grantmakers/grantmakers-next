23885d147e078d6d9d7a27f62ab2750798d09635
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-12-03 13:05:59 -0600

    Add quick placeholder route for /grants

---
+++ apps/web/src/routes/grants/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts"></script>
+
+<div class="min-h-screen">
+  <div class="p-4">Coming soon</div>
+
+  <div class="p-4">In the interim, the legacy grants search remains live - it just has the older grants data</div>
+
+  <div class="p-4">
+    <a class="text-grantmakers-orange" target="_blank" rel="noopener" href="https://www.grantmakers.io/search/grants/"
+      >https://www.grantmakers.io/search/grants/</a
+    >
+  </div>
+</div>
