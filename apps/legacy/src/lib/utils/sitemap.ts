2176853e073dfa7ec036bf916194fa791c8e1cbf
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2025-01-22 10:37:28 -0600

    Fix sitemap namespace per protocol

--- apps/legacy/src/lib/utils/sitemap.ts
+++ apps/legacy/src/lib/utils/sitemap.ts
@@ -21,7 +21,7 @@ export const generateSitemap = (profiles: Profile[]) => {
 
   return new Response(
     `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urlset}
     </urlset>`.trim(),
     {
