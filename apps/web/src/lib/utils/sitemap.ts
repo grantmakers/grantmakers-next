8912765b6c79a3e0bff2e7f5a8cdcc7de0e3764b
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2025-01-26 20:04:01 -0600

    Sitemap routing fix

--- apps/web/src/lib/utils/sitemap.ts
+++ apps/web/src/lib/utils/sitemap.ts
@@ -1,6 +1,9 @@
 import type { Profile } from '@repo/shared/typings/sitemaps';
 import { originProd as hostname, cutoverDate } from '@repo/shared/constants/trustedConstants';
 
+// HACK Hard coding this during the transition from legacy to prod is not ideal
+const profilesVersion = 'v0';
+
 export const chooseDate = (now: string, processed: string, cutover: string) => {
   if (cutover > now) return now; // The cutover date hasn't happened yet, use today's date instead so it's a valid date
   if (processed > cutover) return processed; // It's past the cutover date and there is recent filing update, use that update date
@@ -13,7 +16,7 @@ export const generateSitemap = (profiles: Profile[]) => {
     .map(
       (profile) => `
     <url>
-      <loc>${hostname}/profiles/${profile.ein}-${profile.organization_name_slug}</loc>
+      <loc>${hostname}/profiles/${profilesVersion}/${profile.ein}-${profile.organization_name_slug}</loc>
       <lastmod>${chooseDate(now, profile.last_updated_grantmakers, cutoverDate)}</lastmod>
     </url>`,
     )
