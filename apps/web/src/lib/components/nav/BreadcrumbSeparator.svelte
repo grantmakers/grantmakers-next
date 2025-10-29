13fd4d69f43318cdd78659708f13ab66861fdb59
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2025-10-29 06:56:30 -0500

    Add Breadcrumb component

---
+++ apps/web/src/lib/components/nav/BreadcrumbSeparator.svelte
@@ -0,0 +1,9 @@
+<div class="hidden items-center text-indigo-400 group-data-[sticky=true]:hidden lg:block">
+  <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 shrink-0 text-gray-400 dark:text-gray-500">
+    <path
+      d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+    />
+  </svg>
+</div>
