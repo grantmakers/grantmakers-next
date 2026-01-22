adfd399e0cb484f096e9ca59feaa417ea7e72152
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2026-01-22 10:51:44 -0600

    Quick Svelte 5 upgrade

--- apps/web/src/lib/components/profiles/ContentBoxGrid.svelte
+++ apps/web/src/lib/components/profiles/ContentBoxGrid.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  let { columns = 1, classes = '' } = $props();
+  let { columns = 1, classes = '', children } = $props();
 </script>
 
 <div class="-mx-3 grid grid-cols-1 {columns > 1 ? `md:grid-cols-${columns}` : ''} {classes}">
-  <slot />
+  {@render children()}
 </div>
